a8fa68459053c445049f1ddffbaaf8e92d60889a
Divyanshu Pandey <155071381+CodeCr4cker@users.noreply.github.com>
2025-06-08 18:38:07 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBFBtuIw0HVJl-HYZ9DSP1VZqwXMJli_W8",
+  authDomain: "darknet-chat-f6b5a.firebaseapp.com",
+  projectId: "darknet-chat-f6b5a",
+  storageBucket: "darknet-chat-f6b5a.appspot.com",
+  messagingSenderId: "485072993943",
+  appId: "1:485072993943:web:262edab82d07a87b4733d2",
+  measurementId: "G-2WL2PC8N6H",
 };
 
 const app = initializeApp(firebaseConfig);
